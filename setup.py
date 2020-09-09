ef14fc79f88a4bffe38e12e3a0cb42e3ad40fd79
Bernard Knueven <Bernard.Knueven@nrel.gov>
2020-09-09 17:39:28 -0600

    adding dependency_links for setuptools

--- setup.py
+++ setup.py
@@ -42,5 +42,6 @@ setup(name='prescient',
       package_data={'prescient.downloaders.rts_gmlc_prescient':['runners/*.txt','runners/templates/*']},
       install_requires=['numpy','matplotlib','pandas','scipy','pyomo','six',
                         'pyutilib', 'python-dateutil', 'networkx',
-                        'egret @ git+https://github.com/grid-parity-exchange/Egret.git']
+                        'egret @ git+https://github.com/grid-parity-exchange/Egret.git'],
+      dependency_links=['git+https://github.com/grid-parity-exchange/Egret.git#egg=egret'],
      )
