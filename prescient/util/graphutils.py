f64aceca178cabe8b5364639a32ebb007e3d3b5c
Bernard Knueven <Bernard.Knueven@nrel.gov>
2021-01-14 10:19:35 -0700

    updating daily_cost graph

--- prescient/util/graphutils.py
+++ prescient/util/graphutils.py
@@ -28,45 +28,54 @@ def generate_cost_summary_graph(daily_fixed_costs, daily_generation_costs,
 
     max_cost = max(daily_fixed_costs) + max(daily_generation_costs)
 
-    plt.axis([1,len(daily_fixed_costs),0,max_cost*1.8])
+    fig, ax = plt.subplots(figsize=(16, 8))
+    ax.set_ylim(0,max_cost*1.8)
 
-    plt.bar(list(range(1,len(daily_fixed_costs)+1)), daily_fixed_costs, 1, color = 'k', label="Fixed Costs")
-    plt.bar(list(range(1,len(daily_fixed_costs)+1)), daily_generation_costs, 1, bottom=daily_fixed_costs, color = 'r', label="Variable Costs")
+    ax.bar(list(range(len(daily_fixed_costs))), daily_fixed_costs, 1, color = 'k', label="Fixed Costs")
+    ax.bar(list(range(len(daily_fixed_costs))), daily_generation_costs, 1, bottom=daily_fixed_costs, color = 'r', label="Variable Costs")
 
-    plt.title("Daily Production Costs")
-    plt.ylabel("Cost")
-    plt.xlabel("Day")
-    plt.xticks([i+0.5 for i in range(1,len(daily_fixed_costs)+1)], [str(i) for i in range(1,len(daily_fixed_costs)+1)],rotation=90)
+    ax.set_title("Daily Production Costs")
+    ax.set_ylabel("Cost")
+    ax.set_xlabel("Day")
+    ax.set_xticks(list(range(len(daily_fixed_costs))))
+    ax.set_xticklabels(list(range(1,len(daily_fixed_costs)+1))) # start days at 1
 
-    plt.gca().tick_params(axis='x',pad=15)
-    plt.gca().tick_params(axis='y',pad=15)
-    plt.gca().tick_params(width=1)
-
-    fig = plt.figure(1)
-    ax = fig.add_subplot(111)
-    handles, labels = ax.get_legend_handles_labels()
-    lgd = ax.legend(handles, labels, loc=1, ncol=2, bbox_to_anchor=(1.14, 1.01))
-
-    plt.legend(loc = 1, ncol = 1)
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
     # with the base costs in place, annotate the days with load shedding and over-generation.
     label_height_factor = 1.05
-    for i in range(1,len(daily_fixed_costs)+1):
-        if daily_load_shedding[i-1] > 0.0:
-            event_day = i
-            plt.annotate("Load Shedding", xy=(event_day+0.5, daily_fixed_costs[i-1] + daily_generation_costs[i-1]), arrowprops=dict(arrowstyle='->'), xytext=(event_day, max_cost * label_height_factor))
+    have_arrow = False
+    for i in range(len(daily_fixed_costs)):
+        x_pnt = i
+        y_pnt = daily_fixed_costs[i] + daily_generation_costs[i]
+        xy = (x_pnt, y_pnt)
+        if have_arrow:
+            label_height_factor += 0.05 # add space between days, if we added the last day
+        if label_height_factor > 1.8: # reset the height so we don't go off the screen
+            label_height_factor = 1.05
+        have_arrow = False
+        arrow = dict(arrowstyle='simple')
+        if daily_load_shedding[i] > 0.0:
+            ax.annotate("Load Shedding", xy=xy, arrowprops=(None if have_arrow else arrow),\
+                        xytext=(x_pnt-0.5, max_cost * label_height_factor))
+            have_arrow = True
             label_height_factor += 0.05
-        if daily_over_generation[i-1] > 0.0:
-            event_day = i
-            plt.annotate("Excess Generation", xy=(event_day+0.5, daily_fixed_costs[i-1] + daily_generation_costs[i-1]), arrowprops=dict(arrowstyle='->'), xytext=(event_day, max_cost * label_height_factor))
+        if daily_over_generation[i] > 0.0:
+            ax.annotate("Excess Generation", xy=xy, arrowprops=(None if have_arrow else arrow),\
+                        xytext=(x_pnt-0.5, max_cost * label_height_factor))
+            have_arrow = True
             label_height_factor += 0.05
-        if daily_reserve_shortfall[i-1] > 0.0:
-            event_day = i
-            plt.annotate("Reserve Shortfall", xy=(event_day+0.5, daily_fixed_costs[i-1] + daily_generation_costs[i-1]), arrowprops=dict(arrowstyle='->'), xytext=(event_day, max_cost * label_height_factor))
+        if daily_reserve_shortfall[i] > 0.0:
+            ax.annotate("Reserve Shortfall", xy=xy, arrowprops=(None if have_arrow else arrow),\
+                        xytext=(x_pnt-0.5, max_cost * label_height_factor))
+            have_arrow = True
             label_height_factor += 0.05
-        if daily_curtailments[i-1] > 0.0:
-            event_day = i
-            plt.annotate("Renewables Curtailment", xy=(event_day+0.5, daily_fixed_costs[i-1] + daily_generation_costs[i-1]), arrowprops=dict(arrowstyle='->'), xytext=(event_day, max_cost * label_height_factor))
+        if daily_curtailments[i] > 0.0:
+            ax.annotate("Renewables Curtailment", xy=xy, arrowprops=(None if have_arrow else arrow),\
+                        xytext=(x_pnt-0.5, max_cost * label_height_factor))
+            have_arrow = True
             label_height_factor += 0.05
 
     plt.savefig(os.path.join(output_directory, "daily_costs.png"))
