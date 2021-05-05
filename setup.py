cf7fef06e92ff101ee360c360b06c554e1c56952
Bernard Knueven <Bernard.Knueven@nrel.gov>
2021-05-05 14:03:49 -0600

    adding jupyter

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(name='gridx-prescient',
         },
       package_data={'prescient.downloaders.rts_gmlc_prescient':['runners/*.txt','runners/templates/*']},
       install_requires=['numpy','matplotlib','pandas','scipy','pyomo>=5.7.1',
-                        'pyutilib', 'python-dateutil', 'networkx',
+                        'pyutilib', 'python-dateutil', 'networkx','jupyter',
                         'gridx-egret @ git+https://github.com/grid-parity-exchange/Egret.git'],
       dependency_links=['git+https://github.com/grid-parity-exchange/Egret.git#egg=gridx-egret'],
      )
